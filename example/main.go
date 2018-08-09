62ffbdb8a8057f4b1b09602651ad65497ec7c9b4
Zack Patrick <zpatrick319@Hotmail.com>
2018-08-09 15:28:00 -0700

    add example

---
+++ example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/zpatrick/grid"
+)
+
+func main() {
+	g, err := grid.Parse("grid:facebook:comment:53881")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("GRID: ", g.String())
+	fmt.Println("Namespace:", g.Namespace)
+	fmt.Println("Object Type:", g.ObjectType)
+	fmt.Println("Object ID:", g.ObjectID)
+}
