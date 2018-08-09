f7bdec8922c84cde386a718c589d7dacccb0ff2a
Zack Patrick <zpatrick319@Hotmail.com>
2018-08-09 15:20:50 -0700

    use better error messages

--- grid.go
+++ grid.go
@@ -5,16 +5,17 @@ import (
 	"strings"
 )
 
-// GRID (Global Resource ID) uniquely identify resources.
-// the GRID format is
-//    grid:namespace:object_type:object_id
+// Format of all GRIDs
+const Format = "grid:namespace:object_type:object_id"
+
+// GRID (Global Resource ID) objects are used to uniquely identify resources.
 type GRID struct {
 	Namespace  string
 	ObjectType string
 	ObjectID   string
 }
 
-// Returns a new GRID object
+// New returns a new GRID object.
 func New(namespace, objectType, objectID string) GRID {
 	return GRID{
 		Namespace:  namespace,
@@ -23,17 +24,23 @@ func New(namespace, objectType, objectID string) GRID {
 	}
 }
 
-// Returns a string in GRID in format
+// String returns g in string format.
 func (g GRID) String() string {
 	return fmt.Sprintf("grid:%s:%s:%s", g.Namespace, g.ObjectType, g.ObjectID)
 }
 
-// Attempts to parse a GRID from s
+// Parse attempts to parse a GRID from s.
 func Parse(s string) (GRID, error) {
 	parts := strings.Split(s, ":")
-	if len(parts) != 4 || parts[0] != "grid" {
-		return GRID{}, fmt.Errorf("'%s' is not in GRID format, there should be exactly 4 sections", s)
+	if len(parts) != 4 {
+		return GRID{}, badFormatError(s, "there should be exactly 4 sections")
+	} else if parts[0] != "grid" {
+		return GRID{}, badFormatError(s, "GRIDs must start with 'grid:'")
 	}
 
 	return New(parts[1], parts[2], parts[3]), nil
+}
+
+func badFormatError(s, reason string) error {
+	return fmt.Errorf("'%s' is not in GRID format (%s); %s", s, Format, reason)
 }
