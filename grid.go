34724e529877c59b071fbb4b7f0579d2845316ed
Zack Patrick <zpatrick319@Hotmail.com>
2018-08-09 14:54:39 -0700

    initial commit

---
+++ grid.go
@@ -0,0 +1,38 @@
+package grid
+
+import (
+	"fmt"
+	"strings"
+)
+
+// GRID (Global Resource ID) uniquely identify resources.
+// the GRID format is "grid:namespace:object_type:object_id"
+type GRID struct {
+	Namespace  string
+	ObjectType string
+	ObjectID   string
+}
+
+// Returns a new GRID object
+func New(namespace, objectType, objectID string) GRID {
+	return GRID{
+		Namespace:  namespace,
+		ObjectType: objectType,
+		ObjectID:   objectID,
+	}
+}
+
+// Returns a string in GRID in format
+func (g GRID) String() string {
+	return fmt.Sprintf("grid:%s:%s:%s", g.Namespace, g.ObjectType, g.ObjectID)
+}
+
+// Attempts to parse a GRID from s
+func Parse(s string) (GRID, error) {
+	parts := strings.Split(s, ":")
+	if len(parts) != 4 || parts[0] != "grid" {
+		return GRID{}, fmt.Errorf("'%s' is not in GRID format, there should be exactly 4 sections", s)
+	}
+
+	return New(parts[1], parts[2], parts[3]), nil
+}
---
+++ grid_test.go
@@ -0,0 +1,53 @@
+package grid
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func ExampleGRIDString() {
+	g := New("facebook", "comment", "88532")
+	fmt.Printf("GRID: %s\n", g)
+	// Output:
+	// GRID: grid:facebook:comment:88532
+}
+
+func TestParse(t *testing.T) {
+	cases := map[string]GRID{
+		"grid:namespace:object_type:object_id": New("namespace", "object_type", "object_id"),
+		"grid::object_type:object_id":          New("", "object_type", "object_id"),
+		"grid:::object_id":                     New("", "", "object_id"),
+		"grid:::":                              New("", "", ""),
+	}
+
+	for s, expected := range cases {
+		t.Run(s, func(t *testing.T) {
+			result, err := Parse(s)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t, expected, result)
+		})
+	}
+}
+
+func TestParseError(t *testing.T) {
+	cases := map[string]string{
+		"missing 4 sections":        "",
+		"missing 3 sections":        "object_id",
+		"missing 2 sections":        "object_type:object_id",
+		"missing 1 section":         "namespace:object_type:object_id",
+		"doesn't start with 'grid'": "rgid:namespace:object_type:object_id",
+	}
+
+	for name, input := range cases {
+		t.Run(name, func(t *testing.T) {
+			if _, err := Parse(input); err == nil {
+				t.Fatalf("Error was nil!")
+			}
+		})
+	}
+}
